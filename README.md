1a8f6c835e2471d9df381728aa1ab75dfa2ff95a
Christopher Gadd <gaddman@email.com>
2023-04-08 08:46:14 +1200

    Support ssh key passphrase

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Rundeck plugin to retrieve a file from a remote node.
 ## Description
 Connects to a remote node and retrieves a file, storing it on the Rundeck server. Supports SCP or SFTP.
 
-Tested in RHEL7 and Ubuntu 20.04.
+Tested in RHEL8 and Ubuntu 20.04.
 
 ## Requirements
 - `sshpass`
--- contents/getfile.sh
+++ contents/getfile.sh
@@ -11,57 +11,70 @@
 
 set -e
 
-SOURCE=$RD_CONFIG_SOURCE
-DESTINATION=$RD_CONFIG_DESTINATION
-PROTOCOL=$RD_CONFIG_PROTOCOL
-HOST=$RD_NODE_HOSTNAME
-PROJECT=$RD_JOB_PROJECT
+source="$RD_CONFIG_SOURCE"
+destination="$RD_CONFIG_DESTINATION"
+protocol=$RD_CONFIG_PROTOCOL
+host=$RD_NODE_HOSTNAME
 
-# if no node username defined use job user
+cleanup() {
+  rm -rf "$temp_dir"
+}
+
 if [[ -n "${RD_NODE_USERNAME:-}" ]]; then
-  USERNAME=$(echo $RD_NODE_USERNAME | sed "s/\${job.username}/$RD_JOB_USERNAME/")
+  # Replace ${job.username} variable if supplied.
+  username=${RD_NODE_USERNAME//\$\{job.username\}/"$RD_JOB_USERNAME"}
 else
-  USERNAME="$RD_JOB_USERNAME"
+  # If no node username defined use job user.
+  username="$RD_JOB_USERNAME"
 fi
 
-# Do some basic checks that the destination isn't anywhere stupid
-REALDESTINATION=$(realpath $DESTINATION)
-if [[ "$REALDESTINATION" =~ ^/etc/rundeck/ ]]; then
-  echo "Invalid destination: $DESTINATION"
+# Do some basic checks that the destination isn't anywhere stupid.
+real_destination=$(realpath "$destination")
+if [[ "$real_destination" =~ ^/etc/rundeck/ ]]; then
+  echo "Invalid destination: $destination"
   exit 1
 fi
 
-# If the destination is a directory and doesn't exist, create it
-if [[ "${DESTINATION:(-1)}" == "/" && ! -d "$DESTINATION" ]]; then
-  mkdir -p "$DESTINATION"
+# If the destination is a directory and doesn't exist, create it.
+if [[ "${destination:(-1)}" == "/" && ! -d "$destination" ]]; then
+  mkdir -p "$destination"
 fi
 
-# password may come from storage or an option called 'password'
-# key may come from storage
+temp_dir=$(mktemp -d)
+trap cleanup EXIT
+
+# Password may come from storage or an option called 'password'.
+# Key may come from storage.
 if [[ -n "${RD_SECUREOPTION_PASSWORD:-}" ]]; then
-  PASSWORD="$RD_SECUREOPTION_PASSWORD"
+  echo "$RD_SECUREOPTION_PASSWORD" > "$temp_dir/password"
 elif [[ -n "${RD_OPTION_PASSWORD:-}" ]]; then
-  PASSWORD="$RD_OPTION_PASSWORD"
+  echo "$RD_OPTION_PASSWORD" > "$temp_dir/password"
 elif [[ -n "${RD_CONFIG_PASSWORD_STORAGE_PATH:-}" ]]; then
-  PASSWORD="$RD_CONFIG_PASSWORD_STORAGE_PATH"
+  echo "$RD_CONFIG_PASSWORD_STORAGE_PATH" > "$temp_dir/password"
 elif [[ -n "${RD_CONFIG_SSH_KEY_STORAGE_PATH:-}" ]]; then
-  SSHKEY=$(mktemp)
-  echo "$RD_CONFIG_SSH_KEY_STORAGE_PATH" > $SSHKEY
-  trap 'rm "$SSHKEY"' EXIT
+  echo "$RD_CONFIG_SSH_KEY_STORAGE_PATH"  > "$temp_dir/key"
+  chmod 600 "$temp_dir/key"
+  if [[ -n "${RD_CONFIG_SSH_KEY_PASSPHRASE_STORAGE_PATH:-}" ]]; then
+    echo "$RD_CONFIG_SSH_KEY_PASSPHRASE_STORAGE_PATH"  > "$temp_dir/passphrase"
+  fi
 else
   echo "No password or key provided"
   exit 1
 fi
 
-echo "Copying from $HOST:$SOURCE to $DESTINATION"
+echo "Copying from $host:$source to $destination"
 
-OPTIONS="-r -o StrictHostKeyChecking=No"
-[[ "${RD_JOB_LOGLEVEL:-}" == "DEBUG" ]] && OPTIONS="$OPTIONS -v"
+options="-r -o StrictHostKeyChecking=No"
+[[ "${RD_JOB_LOGLEVEL:-}" == "DEBUG" ]] && options="$options -v"
 
-if [[ -n "${PASSWORD:-}" ]]; then
-  # password auth
-  /usr/bin/sshpass -p $PASSWORD /usr/bin/$PROTOCOL $OPTIONS $USERNAME@$HOST:"$SOURCE" $DESTINATION
+if [[ -e "$temp_dir/password" ]]; then
+  # Password auth.
+  /usr/bin/sshpass -f "$temp_dir/password" /usr/bin/$protocol $options $username@$host:"$source" "$destination"
 else
-  # key auth
-  /usr/bin/$PROTOCOL -i $SSHKEY $OPTIONS $USERNAME@$HOST:"$SOURCE" $DESTINATION
+  # Key auth.
+  if [[ -n "${RD_CONFIG_SSH_KEY_PASSPHRASE_STORAGE_PATH:-}" ]]; then
+    /usr/bin/sshpass -P "Enter passphrase" -f "$temp_dir/passphrase" /usr/bin/$protocol -i "$temp_dir/key" $options $username@$host:"$source" "$destination"
+  else
+    /usr/bin/$protocol -i "$temp_dir/key" $options $username@$host:"$source" "$destination"
+  fi
 fi
